f190a8efb91c614e2a5dcf22d1ff352b3bd084a5
yvettewang <y2554wan@uwaterloo.ca>
2018-08-30 15:41:21 -0400

    Fix bugs
    Using tensorflow 1.10.0 and fixing bugs in data preprocessing.

--- code/ntn.py
+++ code/ntn.py
@@ -3,43 +3,42 @@ import params
 import ntn_input
 import random
 
-# Inference
-# Loss
-# Training
 
-#returns a (batch_size*corrupt_size, 2) vector corresponding to [g(T^i), g(T_c^i)] for all i
-def inference(batch_placeholders, corrupt_placeholder, init_word_embeds, entity_to_wordvec,\
-        num_entities, num_relations, slice_size, batch_size, is_eval, label_placeholders):
+# returns a (batch_size * corrupt_size, 2) vector corresponding to [g(T^i), g(T_c^i)] for all i
+def inference(batch_placeholders, corrupt_placeholder, init_word_embeds, entity_to_wordvec, \
+              num_entities, num_relations, slice_size, batch_size, is_eval, label_placeholders):
     print("Beginning building inference:")
-    #TODO: We need to check the shapes and axes used here!
     print("Creating variables")
-    d = 100 #embed_size
+    d = 100  # embed_size
     k = slice_size
     ten_k = tf.constant([k])
     num_words = len(init_word_embeds)
-    E = tf.Variable(init_word_embeds) #d=embed size
-    W = [tf.Variable(tf.truncated_normal([d,d,k])) for r in range(num_relations)]
-    V = [tf.Variable(tf.zeros([k, 2*d])) for r in range(num_relations)]
-    b = [tf.Variable(tf.zeros([k, 1])) for r in range(num_relations)]
+
+    E = tf.Variable(init_word_embeds)  # d=embed size
+    print("tf E shape: {}".format(E.get_shape()))
+    W = [tf.Variable(tf.truncated_normal([d, d, k])) for r in range(num_relations)]
+    V = [tf.Variable(tf.truncated_normal([k, 2 * d])) for r in range(num_relations)]
+    b = [tf.Variable(tf.truncated_normal([k, 1])) for r in range(num_relations)]
     U = [tf.Variable(tf.ones([1, k])) for r in range(num_relations)]
 
     print("Calcing ent2word")
-    #python list of tf vectors: i -> list of word indices cooresponding to entity i
-    ent2word = [tf.constant(entity_i)-1 for entity_i in entity_to_wordvec]
-    #(num_entities, d) matrix where row i cooresponds to the entity embedding (word embedding average) of entity i
+    # python list of tf vectors: i -> list of word indices cooresponding to entity i
+    ent2word = [tf.cast(tf.constant(entity_i) - 1, tf.int32) for entity_i in entity_to_wordvec]  # 38696
+
     print("Calcing entEmbed...")
-    entEmbed = tf.pack([tf.reduce_mean(tf.gather(E, entword), 0) for entword in ent2word])
-    #entEmbed = tf.truncated_normal([num_entities, d])
-    print(entEmbed.get_shape())
+    # take average embeddings as the entity embedding
+    emtEmbed = tf.stack([tf.reduce_mean(tf.gather(E, entword), 0) for entword in ent2word])
+    # tf.gather(): (3, 100), tf.reduce_mean(): (100,)
+    print(emtEmbed.get_shape())  # (38696, 100)
 
     predictions = list()
     print("Beginning relations loop")
     for r in range(num_relations):
-        print("Relations loop "+str(r))
-        e1, e2, e3 = tf.split(1, 3, tf.cast(batch_placeholders[r], tf.int32)) #TODO: should the split dimension be 0 or 1?
-        e1v = tf.transpose(tf.squeeze(tf.gather(entEmbed, e1, name='e1v'+str(r)),[1]))
-        e2v = tf.transpose(tf.squeeze(tf.gather(entEmbed, e2, name='e2v'+str(r)),[1]))
-        e3v = tf.transpose(tf.squeeze(tf.gather(entEmbed, e3, name='e3v'+str(r)),[1]))
+        print("Relations loop " + str(r))
+        e1, e2, e3 = tf.split(tf.cast(batch_placeholders[r], tf.int32), num_or_size_splits=3, axis=1)
+        e1v = tf.transpose(tf.squeeze(tf.gather(emtEmbed, e1, name='e1v' + str(r)), [1]))
+        e2v = tf.transpose(tf.squeeze(tf.gather(emtEmbed, e2, name='e2v' + str(r)), [1]))
+        e3v = tf.transpose(tf.squeeze(tf.gather(emtEmbed, e3, name='e3v' + str(r)), [1]))
         e1v_pos = e1v
         e2v_pos = e2v
         e1v_neg = e1v
@@ -48,82 +47,68 @@ def inference(batch_placeholders, corrupt_placeholder, init_word_embeds, entity_
         preactivation_pos = list()
         preactivation_neg = list()
 
-        #print("e1v_pos: "+str(e1v_pos.get_shape()))
-        #print("W[r][:,:,slice]: "+str(W[r][:,:,0].get_shape()))
-        #print("e2v_pos: "+str(e2v_pos.get_shape()))
+        print("e1v_pos: " + str(e1v_pos.get_shape()))
+        print("W[r][:,:,slice]: " + str(W[r][:, :, 0].get_shape()))
+        print("e2v_pos: " + str(e2v_pos.get_shape()))
 
-        #print("Starting preactivation funcs")
+        print("Starting preactivation funcs")
         for slice in range(k):
-            preactivation_pos.append(tf.reduce_sum(e1v_pos*tf.matmul(W[r][:,:,slice], e2v_pos), 0))
-            preactivation_neg.append(tf.reduce_sum(e1v_neg*tf.matmul( W[r][:,:,slice], e2v_neg), 0))
+            preactivation_pos.append(tf.reduce_sum(e1v_pos * tf.matmul(W[r][:, :, slice], e2v_pos), 0))
+            preactivation_neg.append(tf.reduce_sum(e1v_neg * tf.matmul(W[r][:, :, slice], e2v_neg), 0))
 
-        preactivation_pos = tf.pack(preactivation_pos)
-        preactivation_neg = tf.pack(preactivation_neg)
+        preactivation_pos = tf.stack(preactivation_pos)
+        preactivation_neg = tf.stack(preactivation_neg)
 
-        temp2_pos = tf.matmul(V[r], tf.concat(0, [e1v_pos, e2v_pos]))
-        temp2_neg = tf.matmul(V[r], tf.concat(0, [e1v_neg, e2v_neg]))
+        temp2_pos = tf.matmul(V[r], tf.concat([e1v_pos, e2v_pos], 0))
+        temp2_neg = tf.matmul(V[r], tf.concat([e1v_neg, e2v_neg], 0))
 
-        #print("   temp2_pos: "+str(temp2_pos.get_shape()))
-        preactivation_pos = preactivation_pos+temp2_pos+b[r]
-        preactivation_neg = preactivation_neg+temp2_neg+b[r]
+        print("   temp2_pos: " + str(temp2_pos.get_shape()))
+        preactivation_pos = preactivation_pos + temp2_pos + b[r]
+        preactivation_neg = preactivation_neg + temp2_neg + b[r]
 
-        #print("Starting activation funcs")
+        print("Starting activation funcs")
         activation_pos = tf.tanh(preactivation_pos)
         activation_neg = tf.tanh(preactivation_neg)
 
         score_pos = tf.reshape(tf.matmul(U[r], activation_pos), num_rel_r)
         score_neg = tf.reshape(tf.matmul(U[r], activation_neg), num_rel_r)
-        #print("score_pos: "+str(score_pos.get_shape()))
+        print("score_pos: " + str(score_pos.get_shape()))
         if not is_eval:
-            predictions.append(tf.pack([score_pos, score_neg]))
+            predictions.append(tf.stack([score_pos, score_neg]))
         else:
-            predictions.append(tf.pack([score_pos, tf.reshape(label_placeholders[r], num_rel_r)]))
-        #print("score_pos_and_neg: "+str(predictions[r].get_shape()))
+            predictions.append(tf.stack([score_pos, tf.reshape(label_placeholders[r], num_rel_r)]))
+            print("score_pos_and_neg: " + str(predictions[r].get_shape()))
 
-
-    #print("Concating predictions")
-    predictions = tf.concat(1, predictions)
-    #print(predictions.get_shape())
+    print("Concating predictions")
+    predictions = tf.concat(predictions, 1)
+    print("predictions: {}".format(predictions.get_shape()))
 
     return predictions
 
 
 def loss(predictions, regularization):
-
     print("Beginning building loss")
-    temp1 = tf.maximum(tf.sub(predictions[1, :], predictions[0, :]) + 1, 0)
+    temp1 = tf.maximum(tf.subtract(predictions[1, :], predictions[0, :]) + 1, 0)
     temp1 = tf.reduce_sum(temp1)
-
     temp2 = tf.sqrt(sum([tf.reduce_sum(tf.square(var)) for var in tf.trainable_variables()]))
-
     temp = temp1 + (regularization * temp2)
-
+    print("loss: {}".format(temp))
     return temp
 
 
 def training(loss, learningRate):
     print("Beginning building training")
-
     return tf.train.AdagradOptimizer(learningRate).minimize(loss)
 
 
 def eval(predictions):
-    print("predictions "+str(predictions.get_shape()))
-    inference, labels = tf.split(0, 2, predictions)
-    #inference = tf.transpose(inference)
-    #inference = tf.concat((1-inference), inference)
-    #labels = ((tf.cast(tf.squeeze(tf.transpose(labels)), tf.int32))+1)/2
-    #print("inference "+str(inference.get_shape()))
-    #print("labels "+str(labels.get_shape()))
+    print("predictions " + str(predictions.get_shape()))  # (batch_size * corrupt_size, 2)
+    prediction, labels = tf.split(predictions, num_or_size_splits=2, axis=0)  # (batch_size * corrupt_size, 1)
+    prediction = tf.concat((1 - tf.transpose(prediction)), tf.transpose(prediction))
+    labels = ((tf.cast(tf.squeeze(tf.transpose(labels)), tf.int32)) + 1) / 2
+    print("predictions " + str(prediction.get_shape()))
+    print("labels " + str(labels.get_shape()))
     # get number of correct labels for the logits (if prediction is top 1 closest to actual)
-    #correct = tf.nn.in_top_k(inference, labels, 1)
+    correct = tf.nn.in_top_k(prediction, labels, 1)
     # cast tensor to int and return number of correct labels
-    #return tf.reduce_sum(tf.cast(correct, tf.int32))
-    return inference, labels
-
-
-
-
-
-
-
+    return tf.reduce_sum(tf.cast(correct, tf.int32))
--- code/ntn_train.py
+++ code/ntn_train.py
@@ -7,48 +7,69 @@ import numpy.matlib
 import random
 import datetime
 
+
 def data_to_indexed(data, entities, relations):
-    entity_to_index = {entities[i] : i for i in range(len(entities))}
-    relation_to_index = {relations[i] : i for i in range(len(relations))}
-    indexed_data = [(entity_to_index[data[i][0]], relation_to_index[data[i][1]],\
-            entity_to_index[data[i][2]]) for i in range(len(data))]
+    entity_to_index = {entities[i]: i for i in range(len(entities))}
+    relation_to_index = {relations[i]: i for i in range(len(relations))}
+    indexed_data = [(entity_to_index[data[i][0]], relation_to_index[data[i][1]], \
+                     entity_to_index[data[i][2]]) for i in range(len(data))]
     return indexed_data
 
-def get_batch(batch_size, data, num_entities, corrupt_size):
+def get_train_batch(batch_size, data, num_entities, corrupt_size):
     random_indices = random.sample(range(len(data)), batch_size)
-    #data[i][0] = e1, data[i][1] = r, data[i][2] = e2, random=e3 (corrupted)
-    batch = [(data[i][0], data[i][1], data[i][2], random.randint(0, num_entities-1))\
-	for i in random_indices for j in range(corrupt_size)]
+    # data[i][0] = e1, data[i][1] = r, data[i][2] = e2, random=e3 (corrupted)
+    batch = [(data[i][0], data[i][1], data[i][2], random.randint(0, num_entities - 1)) \
+             for i in random_indices for j in range(corrupt_size)]
     return batch
 
-def split_batch(data_batch, num_relations):
+def get_test_batch(batch_size, data):
+    random_indices = random.sample(range(len(data)), batch_size)
+    # data[i][0] = e1, data[i][1] = r, data[i][2] = e2
+    batch = [(data[i][0], data[i][1], data[i][2]) for i in random_indices]
+    return batch
+
+def split_train_batch(data_batch, num_relations):
     batches = [[] for i in range(num_relations)]
-    for e1,r,e2,e3 in data_batch:
-        batches[r].append((e1,e2,e3))
+    for e1, r, e2, e3 in data_batch:
+        batches[r].append((e1, e2, e3))
+    return batches
+
+def split_test_batch(data_batch, num_relations):
+    batches = [[] for i in range(num_relations)]
+    for e1, r, e2 in data_batch:
+        batches[r].append((e1, e2))
     return batches
 
 def fill_feed_dict(batches, train_both, batch_placeholders, label_placeholders, corrupt_placeholder):
-    feed_dict = {corrupt_placeholder: [train_both and np.random.random()>0.5]}
+    feed_dict = {corrupt_placeholder: [train_both and np.random.random() > 0.5]}
     for i in range(len(batch_placeholders)):
         feed_dict[batch_placeholders[i]] = batches[i]
         feed_dict[label_placeholders[i]] = [[0.0] for j in range(len(batches[i]))]
     return feed_dict
 
+
 def run_training():
     print("Begin!")
-    #python list of (e1, R, e2) for entire training set in string form
+    # python list of (e1, R, e2) for entire training set in string form
     print("Load training data...")
+    # shape of raw training data: (112581, 3)
     raw_training_data = ntn_input.load_training_data(params.data_path)
+    raw_dev_data = ntn_input.load_dev_data(params.data_path)
+    raw_test_data = ntn_input.load_test_data(params.data_path)
+
     print("Load entities and relations...")
     entities_list = ntn_input.load_entities(params.data_path)
     relations_list = ntn_input.load_relations(params.data_path)
-    #python list of (e1, R, e2) for entire training set in index form
+    num_entities = len(entities_list)  # entity: 38696
+    num_relations = len(relations_list)  # relations: 11
+    # python list of (e1, R, e2) for entire training set in index form
     indexed_training_data = data_to_indexed(raw_training_data, entities_list, relations_list)
-    print("Load embeddings...")
-    (init_word_embeds, entity_to_wordvec) = ntn_input.load_init_embeds(params.data_path)
+    indexed_dev_data = data_to_indexed(raw_dev_data, entities_list, relations_list)
+    indexed_test_data = data_to_indexed(raw_test_data, entities_list, relations_list)
 
-    num_entities = len(entities_list)
-    num_relations = len(relations_list)
+    print("Load embeddings...")
+    # shape of word embeds: 67447, 100; number of entities: 38696
+    (init_word_embeds, entity_to_wordvec) = ntn_input.load_init_embeds(params.data_path)
 
     num_iters = params.num_iter
     batch_size = params.batch_size
@@ -56,38 +77,64 @@ def run_training():
     slice_size = params.slice_size
 
     with tf.Graph().as_default():
-        print("Starting to build graph "+str(datetime.datetime.now()))
-        batch_placeholders = [tf.placeholder(tf.int32, shape=(None, 3), name='batch_'+str(i)) for i in range(num_relations)]
-        label_placeholders = [tf.placeholder(tf.float32, shape=(None, 1), name='label_'+str(i)) for i in range(num_relations)]
+        print("Starting to build graph " + str(datetime.datetime.now()))
+        batch_placeholders = [tf.placeholder(tf.int32, shape=(None, 3), name='batch_' + str(i)) for i in
+                              range(num_relations)]
+        label_placeholders = [tf.placeholder(tf.float32, shape=(None, 1), name='label_' + str(i)) for i in
+                              range(num_relations)]
+        corrupt_placeholder = tf.placeholder(tf.bool, shape=1)
+        train_inference = ntn.inference(batch_placeholders, corrupt_placeholder, init_word_embeds,
+                                  entity_to_wordvec, num_entities, num_relations, slice_size, batch_size,
+                                  False, label_placeholders)
+        test_inference = ntn.inference(batch_placeholders, corrupt_placeholder, init_word_embeds,
+                                  entity_to_wordvec, num_entities, num_relations, slice_size, batch_size,
+                                  True, label_placeholders)
+        train_loss = ntn.loss(train_inference, params.regularization)
+        training = ntn.training(train_loss, params.learning_rate)
 
-        corrupt_placeholder = tf.placeholder(tf.bool, shape=(1)) #Which of e1 or e2 to corrupt?
-        inference = ntn.inference(batch_placeholders, corrupt_placeholder, init_word_embeds, entity_to_wordvec, \
-                num_entities, num_relations, slice_size, batch_size, False, label_placeholders)
-        loss = ntn.loss(inference, params.regularization)
-        training = ntn.training(loss, params.learning_rate)
 
-	# Create a session for running Ops on the Graph.
-	sess = tf.Session()
+        # Create a session for running Ops on the Graph.
+        sess = tf.Session()
 
-	# Run the Op to initialize the variables.
-	init = tf.initialize_all_variables()
-	sess.run(init)
+        # Run the Op to initialize the variables.
+        init = tf.global_variables_initializer()
+        sess.run(init)
         saver = tf.train.Saver(tf.trainable_variables())
+
+        # training
         for i in range(1, num_iters):
-            print("Starting iter "+str(i)+" "+str(datetime.datetime.now()))
-            data_batch = get_batch(batch_size, indexed_training_data, num_entities, corrupt_size)
-            relation_batches = split_batch(data_batch, num_relations)
+            print("Starting iter " + str(i) + " " + str(datetime.datetime.now()))
+            data_train_batch = get_train_batch(batch_size, indexed_training_data, num_entities, corrupt_size)
+            relation_train_batches = split_train_batch(data_train_batch, num_relations)
+            data_dev_batch = get_test_batch(batch_size, indexed_dev_data)
+            relation_dev_batches = split_test_batch(data_dev_batch, num_relations)
+
+            feed_dict_training = fill_feed_dict(relation_train_batches, params.train_both, batch_placeholders,
+                                       label_placeholders, corrupt_placeholder)
+            feed_dict_dev = fill_feed_dict(relation_dev_batches, params.train_both, batch_placeholders,
+                                               label_placeholders, corrupt_placeholder)
+
+            _, train_loss_value, train_eval_value = sess.run([training, train_loss, ntn.eval(train_inference)], feed_dict=feed_dict_training)
+            dev_eval_value = sess.run(ntn.eval(test_inference), feed_dict=feed_dict_dev)
+
+            print("Iter {}, Training data loss = {}".format(i, train_eval_value))
+            print("Iter {}, Dev data loss = {}".format(i, dev_eval_value))
 
             if i % params.save_per_iter == 0:
-                saver.save(sess, params.output_path+"/"+params.data_name+str(i)+'.sess')
+                saver.save(sess, params.output_path + "/" + params.data_name + str(i) + '.sess')
+                print("Model saved at iter {}".format(i))
 
-	    feed_dict = fill_feed_dict(relation_batches, params.train_both, batch_placeholders, label_placeholders, corrupt_placeholder)
-            _, loss_value = sess.run([training, loss], feed_dict=feed_dict)
-
-            #TODO: Eval against dev set?
+        # test
+        data_test_batch = get_test_batch(batch_size, indexed_test_data)
+        relation_test_batches = split_test_batch(data_test_batch, num_relations)
+        feed_dict_testing = fill_feed_dict(relation_test_batches, params.train_both, batch_placeholders,
+                                               label_placeholders, corrupt_placeholder)
+        test_eval_value = sess.run(ntn.eval(test_inference), feed_dict=feed_dict_testing)
+        print("Final Test Accuracy = {}".format(test_eval_value))
 
 def main(argv):
     run_training()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     tf.app.run()
